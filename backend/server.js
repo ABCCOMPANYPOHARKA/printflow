774f64ec08fecb9d6ac0c1e03d775c7a22aac9f1
ABC COMPANY POHARKA <ajaxsihag@gmail.com>
2026-06-06 15:48:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests for all routes
 
 app.use(express.json({ limit: '50mb' }));
 
